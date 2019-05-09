99eade4cd8558adab2aab202775e2490232afe66
DirupT <jjaguirre73@yahoo.com>
2019-05-08 20:37:45 -0500

    Custom logging under each Get endpoint saying the endpoint has been accessed

--- dogs-initial/src/main/java/com/lambdaschool/projectrestdogs/DogController.java
+++ dogs-initial/src/main/java/com/lambdaschool/projectrestdogs/DogController.java
@@ -1,5 +1,7 @@
 package com.lambdaschool.projectrestdogs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +15,13 @@ import java.util.ArrayList;
 @RequestMapping("/dogs")
 public class DogController
 {
+    private static final Logger logger = LoggerFactory.getLogger(DogController.class);
+
     // localhost:8080/dogs/dogs
     @GetMapping(value = "/dogs")
     public ResponseEntity<?> getAllDogs()
     {
+        logger.info("/dogs/dogs accessed");
         return new ResponseEntity<>(ProjectrestdogsApplication.ourDogList.dogList, HttpStatus.OK);
     }
 
@@ -24,6 +29,8 @@ public class DogController
     @GetMapping(value = "/{id}")
     public ResponseEntity<?> getDogDetail(@PathVariable long id)
     {
+        logger.info("/dogs/{id} accessed");
+        logger.debug("/dogs/{id} accessed with the id " + id);
         Dog rtnDog = ProjectrestdogsApplication.ourDogList.findDog(d -> (d.getId() == id));
         return new ResponseEntity<>(rtnDog, HttpStatus.OK);
     }
@@ -32,6 +39,8 @@ public class DogController
     @GetMapping(value = "/breeds/{breed}")
     public ResponseEntity<?> getDogBreeds (@PathVariable String breed)
     {
+        logger.info("/dogs/breeds/{breed} accessed");
+        logger.debug("/dogs/breeds/{breed} accessed with the breed " + breed);
         ArrayList<Dog> rtnDogs = ProjectrestdogsApplication.ourDogList.
                 findDogs(d -> d.getBreed().toUpperCase().equals(breed.toUpperCase()));
         return new ResponseEntity<>(rtnDogs, HttpStatus.OK);
